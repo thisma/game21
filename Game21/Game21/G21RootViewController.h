f161c3263fcf718fcf5c9d53b39fd12d3dc6c5ff
Joshua Smith <ldsaccount@timesmith.ca>
2014-05-29 20:50:22 -0700

    added Root View Controller

---
+++ Game21/Game21/G21RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  G21RootViewController.h
+//  Game21
+//
+//  Created by Joshua E Smith on 2014-05-21.
+//  Copyright (c) 2014 Joshua E Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface G21RootViewController : UIViewController
+
+@end
---
+++ Game21/Game21/G21RootViewController.m
@@ -0,0 +1,53 @@
+//
+//  G21RootViewController.m
+//  Game21
+//
+//  Created by Joshua E Smith on 2014-05-21.
+//  Copyright (c) 2014 Joshua E Smith. All rights reserved.
+//
+
+#import "G21RootViewController.h"
+
+@interface G21RootViewController ()
+
+@end
+
+@implementation G21RootViewController {
+    UIImageView *myImageView;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        myImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Screen Shot 2014-04-05"]];
+        [self.view addSubview:myImageView];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view from its nib.
+    [UIView animateWithDuration:4.0 animations:^{
+        myImageView.center = CGPointMake(100.0, 100.0);
+    }];
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    [UIView animateWithDuration:4.0 animations:^{
+        myImageView.center = [touch locationInView:self.view];
+    }];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
